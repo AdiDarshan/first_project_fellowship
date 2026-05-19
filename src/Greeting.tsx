c1afd6e1414cb71bc6ef8a1ef1a3cfc06dbaa6fc
“AdiDarshan” <gshs1019@gmail.com>
2026-05-19 17:23:24 +0300

    delete files

--- src/Greeting.tsx
+++
@@ -1,9 +0,0 @@
-
-type GreetingProps = {
-    name: string;
-    age: number;
-};
-
-export function Greeting ({name, age} : GreetingProps){
-    return <p>hello, {name}! you are {age} years old</p>;
-}
