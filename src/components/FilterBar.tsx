e624d23e2c969abd403a3b138d39e40f6febe073
“AdiDarshan” <gshs1019@gmail.com>
2026-05-25 17:40:54 +0300

    FilterBar: add filter component
    fix filter bar

---
+++ src/components/FilterBar.tsx
@@ -0,0 +1,33 @@
+import type { Filter } from "../models/types";
+
+type FilterBarProps = {
+  filter: Filter;
+  onChangeFilter: (nextFilter: Filter) => void;
+};
+
+export function FilterBar({ filter, onChangeFilter }: FilterBarProps) {
+  return (
+    <div>
+      <button
+        onClick={() => onChangeFilter("all")}
+        disabled={filter === "all"}
+      >
+        All
+      </button>
+
+      <button
+        onClick={() => onChangeFilter("active")}
+        disabled={filter === "active"}
+      >
+        Active
+      </button>
+
+      <button
+        onClick={() => onChangeFilter("completed")}
+        disabled={filter === "completed"}
+      >
+        Completed
+      </button>
+    </div>
+  );
+}
